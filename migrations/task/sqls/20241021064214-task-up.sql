10161e22cc763f15f7ac2c9dfd68bb44433adfeb
pourquoiaimer <pourquoiaimer@hotmail.com>
2024-12-07 10:46:53 +0800

    先完成到第四關

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -28,7 +28,7 @@ INSERT INTO "USER" (name, email, Role) VALUES
 
 UPDATE "USER"
 SET role = 'COACH'
-WHERE Email IN ('lee2000@hexschooltest.io', 'muscle@hexschooltest.io', 'starplatinum@hexschooltest.io')
+WHERE email IN ('lee2000@hexschooltest.io', 'muscle@hexschooltest.io', 'starplatinum@hexschooltest.io')
 AND role = 'USER';
 
 -- 1-3 刪除：刪除USER 資料表中，用 Email 找到透明人，並刪除該筆資料
@@ -69,22 +69,22 @@ INSERT INTO "CREDIT_PACKAGE" (name, price, credit_amount) VALUES
 
 INSERT INTO "CREDIT_PURCHASE" (user_id, credit_package_id , purchased_credits,price_paid) VALUES
 (
-(SELECT id FROM "USER" WHERE name='王小明'),
-(SELECT id FROM "CREDIT_PACKAGE" WHERE name='14堂組合包方案'),
-(SELECT credit_amount FROM "CREDIT_PACKAGE" WHERE name='14堂組合包方案'),
-(SELECT price FROM "CREDIT_PACKAGE" WHERE name='14堂組合包方案')
+    (SELECT id FROM "USER" WHERE name='王小明'),
+    (SELECT id FROM "CREDIT_PACKAGE" WHERE name='14堂組合包方案'),
+    (SELECT credit_amount FROM "CREDIT_PACKAGE" WHERE name='14堂組合包方案'),
+    (SELECT price FROM "CREDIT_PACKAGE" WHERE name='14堂組合包方案')
 ),
 (
-(SELECT id FROM "USER" WHERE name='王小明'),
-(SELECT id FROM "CREDIT_PACKAGE" WHERE name='21堂組合包方案'),
-(SELECT credit_amount FROM "CREDIT_PACKAGE" WHERE name='21堂組合包方案'),
-(SELECT price FROM "CREDIT_PACKAGE" WHERE name='21堂組合包方案')
+    (SELECT id FROM "USER" WHERE name='王小明'),
+    (SELECT id FROM "CREDIT_PACKAGE" WHERE name='21堂組合包方案'),
+    (SELECT credit_amount FROM "CREDIT_PACKAGE" WHERE name='21堂組合包方案'),
+    (SELECT price FROM "CREDIT_PACKAGE" WHERE name='21堂組合包方案')
 ),
 (
-(SELECT id FROM "USER" WHERE name='好野人'),
-(SELECT id FROM "CREDIT_PACKAGE" WHERE name='14堂組合包方案'),
-(SELECT credit_amount FROM "CREDIT_PACKAGE" WHERE name='14堂組合包方案'),
-(SELECT price FROM "CREDIT_PACKAGE" WHERE name='14堂組合包方案')
+    (SELECT id FROM "USER" WHERE name='好野人'),
+    (SELECT id FROM "CREDIT_PACKAGE" WHERE name='14堂組合包方案'),
+    (SELECT credit_amount FROM "CREDIT_PACKAGE" WHERE name='14堂組合包方案'),
+    (SELECT price FROM "CREDIT_PACKAGE" WHERE name='14堂組合包方案')
 );
 
 
@@ -101,17 +101,66 @@ INSERT INTO "CREDIT_PURCHASE" (user_id, credit_package_id , purchased_credits,pr
     -- 2. 將用戶`肌肉棒子`新增為教練，並且年資設定為2年
     -- 3. 將用戶`Q太郎`新增為教練，並且年資設定為2年
 
+INSERT INTO "COACH" (user_id, experience_years) VALUES
+(
+    (SELECT id FROM "USER" WHERE email ='lee2000@hexschooltest.io'),2
+),
+(
+    (SELECT id FROM "USER" WHERE name ='肌肉棒子'),2
+),
+(
+    (SELECT id FROM "USER" WHERE name ='Q太郎'),2
+)
 -- 3-2. 新增：承1，為三名教練新增專長資料至 `COACH_LINK_SKILL` ，資料需求如下：
     -- 1. 所有教練都有 `重訓` 專長
     -- 2. 教練`肌肉棒子` 需要有 `瑜伽` 專長
     -- 3. 教練`Q太郎` 需要有 `有氧運動` 與 `復健訓練` 專長
 
+INSERT INTO "COACH_LINK_SKILL" (coach_id,skill_id) VALUES
+(
+    (SELECT id FROM "COACH" WHERE user_id=(SELECT id FROM "USER" WHERE name='李燕容')),
+    (SELECT id FROM "SKILL" WHERE name='重訓')
+),
+
+(
+    (SELECT id FROM "COACH" WHERE user_id=(SELECT id FROM "USER" WHERE name='肌肉棒子')),
+    (SELECT id FROM "SKILL" WHERE name='重訓'))
+ ,
+(
+    (SELECT id FROM "COACH" WHERE user_id=(SELECT id FROM "USER" WHERE name='Q太郎')),
+    (SELECT id FROM "SKILL" WHERE name='重訓')
+),
+(
+    (SELECT id FROM "COACH" WHERE user_id=(SELECT id FROM "USER" WHERE name='肌肉棒子')),
+    (SELECT id FROM "SKILL" WHERE name='瑜伽')
+),
+(
+    (SELECT id FROM "COACH" WHERE user_id=(SELECT id FROM "USER" WHERE  name='Q太郎')),
+    (SELECT id FROM "SKILL" WHERE name='有氧運動')
+),
+(
+    (SELECT id FROM "COACH" WHERE	user_id=(SELECT id FROM "USER" WHERE  name='Q太郎')),
+    (SELECT id FROM "SKILL" WHERE name='復健訓練')
+);
+
+
 -- 3-3 修改：更新教練的經驗年數，資料需求如下：
     -- 1. 教練`肌肉棒子` 的經驗年數為3年
     -- 2. 教練`Q太郎` 的經驗年數為5年
 
+UPDATE "COACH"
+SET experience_years = 3
+WHERE user_id =(select id from "USER" where name='肌肉棒子');
+
+UPDATE "COACH"
+SET experience_years = 5
+WHERE user_id =(select id from "USER" where name='Q太郎');
+
 -- 3-4 刪除：新增一個專長 空中瑜伽 至 SKILL 資料表，之後刪除此專長。
 
+INSERT INTO "SKILL" (name) VALUES ('空中瑜伽');
+
+DELETE FROM "SKILL" WHERE name='空中瑜伽';
 
 --  ████████  █████   █    █   █ 
 --    █ █   ██    █  █     █   █ 
@@ -129,6 +178,17 @@ INSERT INTO "CREDIT_PURCHASE" (user_id, credit_package_id , purchased_credits,pr
     -- 5. 授課結束時間`end_at`設定為2024-11-25 16:00:00
     -- 6. 最大授課人數`max_participants` 設定為10
     -- 7. 授課連結設定`meeting_url`為 https://test-meeting.test.io
+
+INSERT INTO "COURSE" (user_id,skill_id,name,start_at,end_at,max_participants,meeting_url)values
+( 
+    (SELECT id FROM "USER" WHERE name='李燕容'),
+    (SELECT id FROM "SKILL" WHERE name='重訓'),
+    '重訓基礎課',
+    '2024-11-25 14:00:00',
+    '2024-11-25 16:00:00',
+    10,
+    'https://test-meeting.test.io'
+);
 
 
 -- ████████  █████   █    █████ 
